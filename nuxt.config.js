a34a5e4309378c31a831529490f2d30489c98020
James Middleton <hello@jamesmiddleton.me>
2020-06-09 11:00:32 +0100

    feat: Make all text antialiased and add lang="en"

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,11 @@ export default {
   ** Headers of the page
   */
   head: {
-    title: process.env.npm_package_name || '',
+    htmlAttrs: {
+      lang: 'en',
+      class: 'antialiased',
+    },
+    title: 'eden labs',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -13,12 +17,12 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
   },
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: '#555' },
   /*
   ** Global CSS
   */
